2e742941904d05105a8af470c5e72226a090d1f3
md-ahmodullah <contactme.ahmod@gmail.com>
2025-01-28 00:27:57 +0600

    created donation get and post api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -7,6 +9,27 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173", "https://edulink-f2125.web.app"],
+//     credentials: true,
+//   })
+// );
+// app.use(cookieParser());
+// const verifyToken = (req, res, next) => {
+//   const token = req.cookies?.token;
+
+//   if (!token) {
+//     return res.status(401).send({ message: "Unauthorized Access!" });
+//   }
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "Unauthorized Access!" });
+//     }
+//     next();
+//   });
+// };
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uqv4w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,6 +44,41 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const userCollection = client.db("registerDB").collection("userCollection");
+
+    const donationRequestCollection = client
+      .db("requestDB")
+      .collection("donationRequest");
+
+    app.get("/donationRequest", async (req, res) => {
+      const { requesterEmail, status } = req.query;
+
+      const query = {};
+
+      if (status) {
+        query.status = status;
+      }
+
+      if (requesterEmail) {
+        query.requesterEmail = requesterEmail;
+      }
+      try {
+        const myDonation = await donationRequestCollection
+          .find(query)
+          .toArray();
+        res.json(myDonation);
+      } catch (error) {
+        console.error("Error fetching submissions:", error);
+        res.status(500).send({ message: "Failed to fetch submissions" });
+      }
+    });
+
+    app.post("/donationRequest", async (req, res) => {
+      const newRequest = req.body;
+      const result = await donationRequestCollection.insertOne(newRequest);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
